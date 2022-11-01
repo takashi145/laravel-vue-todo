76b8fd8b2b28262f46b914894cca5c2cf1787cc5
takashi145 <t.shuhei0203@gmail.com>
2022-11-01 17:31:10 +0900

    task show

--- resources/js/Pages/task/Index.vue
+++ resources/js/Pages/task/Index.vue
@@ -5,9 +5,13 @@ import axios from 'axios';
 
 const tasks = ref([]);
 
+const taskDetail = ref({});
+
 const isLoading = ref(false);
 
 const openModal = ref(false);
+
+const showTaskModal = ref(false);
 
 const form = reactive({
   title: '',
@@ -43,6 +47,18 @@ const createTask = async () => {
         form.deadline = ''
         openModal.value = false
       })
+  }catch(e) {
+    console.log(e)
+  }
+}
+
+const showTask = async (id) => {
+  try {
+    await axios.get(`/api/task/${id}`)
+      .then(res => {
+        taskDetail.value = res.data
+      });
+    showTaskModal.value = true;
   }catch(e) {
     console.log(e)
   }
@@ -103,6 +119,46 @@ const deleteTask = async (id) => {
         </div>
     </div>
 
+    <div v-if="showTaskModal" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+        <div class="fixed inset-0 bg-black opacity-50"></div>
+        <div class="mx-auto mt-20 relative p-4 w-full md:max-w-3xl h-full md:h-auto">
+            <div class="relative bg-gray-100 rounded shadow">
+                <button type="button" @click="showTaskModal = false" class="absolute top-3 right-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+                <div class="py-6 px-6 lg:px-8">
+                    <h3 class="mb-8 text-2xl font-medium text-gray-800">タスク詳細</h3>
+                    <div class="space-y-8">
+                        <div>
+                            <label for="title" class="block mb-2 text-lg font-medium text-gray-900">Title <span class="text-red-500">*</span></label>
+                            <div name="title" id="title" class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3">{{ taskDetail.title }}</div>
+                        </div>
+                        <div>
+                            <label for="description" class="block mb-2 text-lg font-medium text-gray-900">Description</label>
+                            <div name="description" id="description" class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 h-40">{{ taskDetail.description }}</div>
+                        </div>
+                        <div>
+                            <label class="block mb-2 text-lg font-medium text-gray-900">Status</label>
+                            <div v-if="taskDetail.completed" class="bg-green-400 border border-green-300 text-white text-sm font-bold rounded-lg p-3">
+                              完了
+                            </div>
+                            <div v-else class="bg-red-400 border border-red-300 text-white text-sm font-bold rounded-lg p-3">
+                              未完了
+                            </div>
+                        </div>
+                        <div>
+                          <label for="deadline" class="block mb-2 text-lg font-medium text-gray-900">Deadline<span class="text-red-500"> *</span></label>
+                          <div name="deadline" id="deadline" class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3">{{ taskDetail.deadline }}</div>
+                        </div>
+                        <div class="text-center">
+                          <button type="button" @click="showTaskModal = false" class="w-1/2 mx-3 text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div v-if="!isLoading">
       <div class="fixed inset-0 bg-black opacity-10"></div>
       <div class="flex justify-center">
@@ -134,7 +190,9 @@ const deleteTask = async (id) => {
             <tbody>
               <tr v-for="task in tasks" :key="task.id">
                 <td class="px-4 py-3">{{ task.id }}</td>
-                <td class="px-4 py-3">{{ task.title }}</td>
+                <td class="px-4 py-3">
+                  <button @click="showTask(task.id)">{{ task.title }}</button>
+                </td>
                 <td class="px-4 py-3">
                   <form @submit.prevent="toggleStatus(task.id)">
                     <button type="submit">
